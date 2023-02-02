db25837296d5eea61206340c294fe0528e972391
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2023-02-02 19:27:59 +0100

    task 3

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 '''
 type-annotated function
-'''
-import math   
+''' 
 
 def floor(n: float) -> int:
     '''function  takes a float n and floors it
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+type-annotated function
+''' 
+
+def to_str(n: float) -> str:
+    '''function  takes a float n and returns a str
+    '''
+    return str(n)
