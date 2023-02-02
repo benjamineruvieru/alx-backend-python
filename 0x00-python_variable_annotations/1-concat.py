c483570eb498758512c34adfa6d664640b65de41
Benjamin Eruvieru <benjamineruvieru@gmail.com>
2023-02-02 19:25:21 +0100

    task 3

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,7 +4,7 @@ type-annotated function
 '''
 
 
-def add(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     '''function  takes a string str1 and a string str2
     as arguments and returns a concatenated string
     '''
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+type-annotated function
+'''
+import math   
+
+def floor(n: float) -> int:
+    '''function  takes a float n and floors it
+    '''
+    return math.floor(n)
